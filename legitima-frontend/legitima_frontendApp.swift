6a205d942ec1be2b1f6ec5a8a1d3f86a9286c5f1
Milehana Joseph <milehana.joseph@gmail.com>
2026-02-11 22:17:27 +0100

    Rebase project on clean Xcode template

---
+++ legitima-frontend/legitima_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  legitima_frontendApp.swift
+//  legitima-frontend
+//
+//  Created by MilehanaLiveComm on 11/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct legitima_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
