9296ea725eb83962f262f9639106c3f0f9822850
Milehana Joseph <milehana.joseph@gmail.com>
2026-07-21 11:50:27 +0200

    Add CV prefill to premium parcours step

--- legitima-frontend/Models/PremiumPreparationDraft.swift
+++ legitima-frontend/Models/PremiumPreparationDraft.swift
@@ -3,10 +3,14 @@ import Foundation
 
 final class PremiumPreparationDraft: ObservableObject {
     @Published var anchorRole: String = ""
+    @Published var careerKeySteps: String = ""
+    @Published var careerTransitions: String = ""
     @Published var baseAnalysis: AnalysisResponse?
 
     func reset() {
         anchorRole = ""
+        careerKeySteps = ""
+        careerTransitions = ""
         baseAnalysis = nil
     }
 }
